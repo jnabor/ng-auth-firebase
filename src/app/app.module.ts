39d0cb0259036f95d89be83bda695890c9bfa367
Jayson Nabor <sonabstudios@gmail.com>
2018-01-27 07:45:25 +0800

    base project with material design integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
+/* Angular material */
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+      MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
